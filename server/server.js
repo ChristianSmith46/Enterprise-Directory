f02ae1cf55a95eb139e5ad75f1fe1c5f46b01666
Andy Lee <choongho98@gmail.com>
2023-10-11 10:42:16 -0400

    mongo

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 
-const port = 3000
+const port = 3000;
+
+mongoose.connect(
+  "mongodb+srv://hackathon:0l7bgp3FPmaZ9DsA@cluster0.nvcnai8.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const app = express();
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
+  console.log(`Listening on port ${port}`);
 });
