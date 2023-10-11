f7d450b85bf3c509e2107d7807db01843442550f
Christian Smith <christian.46.smith@gmail.com>
2023-10-11 16:40:45 -0400

    begin employee salary search

--- server/dao.js
+++ server/dao.js
@@ -10,8 +10,12 @@ module.exports = {
             const userData = { name, email, password, phoneNumber, managerID, roleID, salary, locationID };
             const newUser = await User.create(userData);
             const token = signToken({ email, _id: newUser._id });
-            res.send({ _id: newUser._id, token });
+            res.send({ success: true, _id: newUser._id, token });
         } catch (err) {
+            if (err.name === 'MongoServerError' && err.code === 11000) {
+                // Duplicate username
+                return res.status(422).send({ success: false, message: 'Email already exists.' });
+            }
             console.error(err);
             res.status(500).send({ error: "Server error" });
         }
@@ -24,7 +28,7 @@ module.exports = {
             const { _id } = req.user
             const user = await User.findById(_id, { password: 0, __v: 0 });
             console.log(user);
-            res.send({ user });
+            res.send({ success: true, user });
         } catch (err) {
             console.error(err);
             res.status(500).send({ error: "Server error" });
@@ -33,11 +37,12 @@ module.exports = {
     login: async (req, res) => {
         try {
             const { email, password } = req.body;
+            // Use bcrypt to check passwords later on
             const user = await User.findOne({ email, password });
             console.log(user)
-            if (!user) return res.status(400).send({ error: "email or password invalid" });
+            if (!user) return res.status(400).send({ success: false, error: "email or password invalid" });
             const token = signToken({ email, _id: user._id });
-            res.send({ email, token });
+            res.send({ success: true, email, token });
         } catch (err) {
             console.error(err);
             res.status(500).send({ error: "Server error" });
@@ -45,5 +50,30 @@ module.exports = {
     },
     deleteUser: (req, res) => {
         res.send({ error: "Requires Completion" })
+    },
+    getDirectReports: async (req, res) => {
+        try {
+            const { _id } = req.user
+            //TODO: Fix managerID once done testing
+            const directReports = await User.find({ managerID: 1 }, { phoneNumber: 0, password: 0, __v: 0, email: 0 });
+            console.log(directReports);
+            res.send({ success: true, directReports });
+        } catch (err) {
+            console.error(err);
+            res.status(500).send({ error: "Server error" });
+        }
+    },
+    getOneDirectRport: async (req, res) => {
+        try {
+            const { _id } = req.user;
+            const { employeeID } = req.params;
+            //TODO: Fix managerID once done testing
+            const employee = await User.findOne({ managerID: 1, _id: employeeID }, { phoneNumber: 0, password: 0, __v: 0, email: 0 });
+            console.log(employee);
+            res.send({ success: true, employee });
+        } catch (err) {
+            console.error(err);
+            res.status(500).send({ error: "Server error" });
+        }
     }
 };
