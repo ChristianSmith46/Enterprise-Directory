3ca5566b7563b57f614c3f5570e3713a30ef8ed9
Andy Lee <choongho98@gmail.com>
2023-10-11 13:55:59 -0400

    added create manager

--- server/seed.js
+++ server/seed.js
@@ -4,32 +4,56 @@ const User = require("./models/User");
 
 const USERS = Array.from({ length: 100 }, createRandomUser);
 
+const Manager = Array.from({ length: 10 }, createRandomManager);
+
 const connectDB = async () => {
   try {
     await mongoose.connect(
       "mongodb+srv://hackathon:0l7bgp3FPmaZ9DsA@cluster0.nvcnai8.mongodb.net/?retryWrites=true&w=majority"
     );
     console.log("connected to db");
-    User.create(USERS);
+    const outputManager = await User.create(Manager);
+    console.log(outputManager);
+    const outputUser = await User.create(USERS);
   } catch (error) {
     console.error(error);
   }
 };
 connectDB();
 
+function createRandomPhoneNumber() {
+  const areaCode = faker.number.int({ min: 100, max: 999 });
+  const prefix = faker.number.int({ min: 100, max: 999 });
+  const lineNumber = faker.number.int({ min: 1000, max: 9999 });
+
+  return `${areaCode}-${prefix}-${lineNumber}`;
+}
+
 function createRandomUser() {
   return {
     name: faker.person.fullName(),
-    phoneNumber: faker.phone.number(),
+    phoneNumber: createRandomPhoneNumber(),
     roleID: faker.number.int({ max: 3 }),
     locationID: faker.number.int({ max: 3 }),
-    salary: faker.finance.amount(50000, 200000, 0),
+    salary: faker.finance.amount(50000, 100000, 0),
     email: faker.internet.email(),
     password: faker.internet.password(),
     managerID: faker.number.int({ max: 3 }),
   };
 }
 
-console.log(USERS);
+function createRandomManager() {
+  return {
+    name: faker.person.fullName(),
+    phoneNumber: createRandomPhoneNumber(),
+    roleID: 1,
+    locationID: faker.number.int({ max: 3 }),
+    salary: faker.finance.amount(100000, 250000, 0),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+}
+
+// console.log(Manager);
 
 module.exports = USERS;
