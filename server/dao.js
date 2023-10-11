860361cd9b5357164e15688c585be1f3731deeae
Christian Smith <christian.46.smith@gmail.com>
2023-10-11 10:47:49 -0400

    begin routes setup

---
+++ server/dao.js
@@ -0,0 +1,13 @@
+
+
+export function createUser(req, res) {
+    res.send({ error: "Requires Completion" })
+}
+
+export function updateUser(req, res) {
+    res.send({ error: "Requires Completion" })
+}
+
+export function login(req, res) {
+    res.send({ error: "Requires completion" })
+}
---
+++ server/utils/auth.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken');
+
+// set token secret and expiration date
+const secret = process.env.JWT_SECRET || 'TempSecret';
+const expiration = '30d';
+
+export function authMiddleware({ req }) {
+    // allows token to be sent via  req.query or headers
+    let token = req.query.token || req.headers.authorization;
+
+    // ["Bearer", "<tokenvalue>"]
+    if (req.headers.authorization) {
+        token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+        return req;
+    }
+
+    // verify token and get user data out of it
+    try {
+        const { data } = jwt.verify(token, secret, { maxAge: expiration });
+        req.user = data;
+    } catch {
+        console.log('Invalid token');
+    }
+
+    // send to next endpoint
+    return req;
+}
+
+export function signToken({ email, _id }) {
+    const payload = { email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+}
